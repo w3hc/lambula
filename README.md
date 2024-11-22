a0d9f7154b65105234a475cd70ab0a709345163a
julienbrg <julien@strat.cc>
2024-11-22 18:11:00 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,33 +1,25 @@
-## Getting Started
+# Lambula
 
-This example demonstrates how to use thirdweb Auth with NextJS.
-
-To run the project, first clone this repository, and then run one of the following commands to install the dependencies:
+## Install
 
 ```bash
-npm install
-# or
-yarn
+pnpm i
 ```
+
+## Run
+
+Create a `.env` file:
+
+```
+cp .env.example .env
+```
+
+Add your own keys in the `.env` file (you can get it in your [Reown dashboard](https://cloud.reown.com/)), then:
 
 ```bash
-npm run dev
-# or
-yarn dev
+pnpm dev
 ```
 
-Now, you can navigate to [http://localhost:3000](http://localhost:3000) to visit the client side page where you can connect a wallet, sign-in with ethereum and view the payload, and use the payload to authenticate with the backend.
+## Support
 
-## Learn More
-
-To learn more about thirdweb, take a look at the following resources:
-
-- [thirdweb Auth Documentation](https://docs.thirdweb.com/auth) - learn about thirdweb Auth.
-- [thirdweb React Documentation](https://docs.thirdweb.com/react) - learn about our React SDK.
-- [thirdweb Portal](https://docs.thirdweb.com) - check our guides and development resources.
-
-You can check out [the thirdweb GitHub organization](https://github.com/thirdweb-dev) - your feedback and contributions are welcome!
-
-## Join our Discord!
-
-For any questions, suggestions, join our discord at [https://discord.gg/thirdweb](https://discord.gg/thirdweb).
+You can contact me via [Element](https://matrix.to/#/@julienbrg:matrix.org), [Farcaster](https://warpcast.com/julien-), [Telegram](https://t.me/julienbrg), [Twitter](https://twitter.com/julienbrg), [Discord](https://discordapp.com/users/julienbrg), or [LinkedIn](https://www.linkedin.com/in/julienberanger/).
---
+++ src/components/Header.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import {
+  Flex,
+  useColorModeValue,
+  Spacer,
+  Heading,
+  Box,
+  Link,
+  Icon,
+  Button,
+  MenuList,
+  MenuItem,
+  Menu,
+  MenuButton,
+  IconButton,
+} from '@chakra-ui/react'
+import { LinkComponent } from './LinkComponent'
+import { ThemeSwitcher } from './ThemeSwitcher'
+import { HeadingComponent } from './HeadingComponent'
+import { SITE_NAME } from '../utils/config'
+import { FaGithub } from 'react-icons/fa'
+import { Web3Modal } from '../context/web3modal'
+import { HamburgerIcon } from '@chakra-ui/icons'
+
+interface Props {
+  className?: string
+}
+
+export function Header(props: Props) {
+  const className = props.className ?? ''
+
+  return (
+    <Flex
+      as="header"
+      className={className}
+      bg={useColorModeValue('gray.100', 'gray.900')}
+      px={4}
+      py={5}
+      mb={8}
+      alignItems="center">
+      <LinkComponent href="/" invisible>
+        <Heading as="h1" size="md">
+          {SITE_NAME}
+        </Heading>
+      </LinkComponent>
+
+      <Spacer />
+      <Menu>
+        <MenuButton as={IconButton} aria-label="Options" icon={<HamburgerIcon />} size={'sm'} mr={4} />
+        <MenuList>
+          <LinkComponent href="/" invisible>
+            <MenuItem fontSize="md">Home</MenuItem>
+          </LinkComponent>
+          <LinkComponent href="/new" invisible>
+            <MenuItem fontSize="md">New</MenuItem>
+          </LinkComponent>
+        </MenuList>
+      </Menu>
+      <Flex alignItems="center" gap={4}>
+        <w3m-button />
+        <Flex alignItems="center">
+          <ThemeSwitcher />
+          <Box mt={2} ml={4}>
+            <Link href="https://github.com/w3hc/lambula" isExternal>
+              <Icon as={FaGithub} boxSize={5} _hover={{ color: 'blue.500' }} />
+            </Link>
+          </Box>
+        </Flex>
+      </Flex>
+    </Flex>
+  )
+}
